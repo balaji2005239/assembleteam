1e4a2eecddbfe296699fdcc71488bffc950717b9
P Balaji Subramanian <balajisubramanian.p2023@vitstudent.ac.in>
2025-07-11 23:35:17 +0530

    configure api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import toast from 'react-hot-toast';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://assemble-backend-4soi.onrender.com/api';
+
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
